c19841a690195322a85baae5ab5e921717513ed0
debasishb77 <debasishbhola77@gmail.com>
2024-07-22 16:43:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css';
+function App() {
+  
+  return (
+    <div className=''>
+      <p className='ch'>Hello</p>
+      <p className="text-6xl font-bold underline">
+      Hello world!
+    </p>
+    </div>
+  )
+}
+
+export default App
