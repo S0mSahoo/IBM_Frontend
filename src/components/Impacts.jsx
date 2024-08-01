76fe6e4cb81cee5095ea24253c49fc795c3ff68d
biswa <biswabaibhab007@gmail.com>
2024-08-01 18:57:12 +0530

    temporary commit for impacts

--- src/components/Impacts.jsx
+++ src/components/Impacts.jsx
@@ -4,9 +4,9 @@ import reviews from "../data/impactStories";
 
 const Impacts = () => {
   return( 
-  <div className="bg-img flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200 overflow-x-hidden">
+  <div className="bg-img flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200 overflow-x-hidden overflow-y-scroll">
     <div className="text-center">
-      <h1 className="text-4xl font-bold">Impact Stories</h1>
+      <h1 className="text-4xl font-bold pt-60">Impact Stories</h1>
       <div className="bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto"></div>
     </div>
     <Impacting reviews={reviews}/>
