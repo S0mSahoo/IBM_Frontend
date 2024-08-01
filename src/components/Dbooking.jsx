40dca27575d24a0901200d58559b164e5d206b4b
Subodha kumar Dhal <subodhadhal2021@gmail.com>
2024-08-02 00:57:18 +0530

    image added

--- src/components/Dbooking.jsx
+++ src/components/Dbooking.jsx
@@ -6,7 +6,6 @@ import '../css/DoctorsBooking.css';
 import icon from '../assets/data/doctor.jpeg';
 
 
-
 const Dbooking = () => {
 
   
@@ -23,7 +22,7 @@ const Dbooking = () => {
       id: 1,
       name: 'Dr. John Doe',
       specialization: 'General Physician',
-      image: 'https://img.freepik.com/free-photo/doctor-smiling-with-arms-crossed_23-2148170980.jpg',
+      image: 'https://media.istockphoto.com/id/690210372/photo/smiling-male-doctor-with-arms-crossed-in-hospital.jpg?s=612x612&w=0&k=20&c=xIxP4kNxRDRzE6KfMp2a56O3l6Z-JvF1k3wuKKGJsmQ=',
       alt: 'Dr. John Doe',
       specialty: 'General Consultation',
       description: 'Our experienced general physicians provide comprehensive health care for all age groups.',
@@ -33,7 +32,7 @@ const Dbooking = () => {
       id: 2,
       name: 'Dr. Sarah Williams',
       specialization: 'Pediatrician',
-      image: 'https://img.freepik.com/free-photo/portrait-smiling-female-doctor_23-2148849776.jpg',
+      image: 'https://img.freepik.com/premium-photo/attractive-female-doctor-white-coat-glasses_88135-6126.jpg?w=900',
       alt: 'Dr. Sarah Williams',
       specialty: 'Pediatric Care',
       description: 'Specialized care for infants, children, and adolescents by our expert pediatricians.',
@@ -43,7 +42,7 @@ const Dbooking = () => {
       id: 3,
       name: 'Dr. Alan Brown',
       specialization: 'Dermatologist',
-      image: 'https://img.freepik.com/free-photo/happy-doctor_23-2148153982.jpg',
+      image: 'https://img.freepik.com/free-photo/attractive-young-male-nutriologist-lab-coat-smiling-against-white-background_662251-2960.jpg?t=st=1722536928~exp=1722540528~hmac=8de652f42231c4d0a20c2c32480f6c6ccc5df6bc8fb290e9a4a92e5ceee753b1&w=900',
       alt: 'Dr. Alan Brown',
       specialty: 'Dermatology',
       description: 'Advanced skin care treatments and procedures by our skilled dermatologists.',
@@ -53,7 +52,7 @@ const Dbooking = () => {
       id: 4,
       name: 'Dr. Jane Smith',
       specialization: 'Cardiologist',
-      image: 'https://img.freepik.com/free-photo/doctor-smiling-with-stethoscope_23-2148887824.jpg',
+      image: 'https://media.istockphoto.com/id/1207212127/photo/experience-you-can-trust.jpg?s=612x612&w=0&k=20&c=V0sNP_j9l3QQKwQ3dU4d4cJGgvdqceuXCj3__6m2So4=',
       alt: 'Dr. Jane Smith',
       specialty: 'Cardiology',
       description: 'Expert cardiac care including diagnosis, treatment, and prevention of heart conditions.',
@@ -63,7 +62,7 @@ const Dbooking = () => {
       id: 5,
       name: 'Dr. Michael Johnson',
       specialization: 'Orthopedist',
-      image: 'https://img.freepik.com/free-photo/portrait-happy-smiling-doctor_23-2148887315.jpg',
+      image: 'https://img.freepik.com/free-photo/attractive-medical-professional-uniform-standing-with-arms-crossed-against-isolated-background_662251-416.jpg?w=740&t=st=1722536442~exp=1722537042~hmac=de1514445b16db931ed8ff6f2203c353a08ab4395e43bdb86ec1d374d42dfdce',
       alt: 'Dr. Michael Johnson',
       specialty: 'Orthopedics',
       description: 'Comprehensive care for bone, joint, and muscle conditions by our orthopedic specialists.',
@@ -73,7 +72,7 @@ const Dbooking = () => {
       id: 6,
       name: 'Dr. Emily Davis',
       specialization: 'Ophthalmologist',
-      image: 'https://img.freepik.com/free-photo/portrait-smiling-female-doctor_23-2148849776.jpg',
+      image: 'https://img.freepik.com/premium-photo/studio-shot-young-muscular-man-doctor-against-white-backgroun_251136-51166.jpg?w=996',
       alt: 'Dr. Emily Davis',
       specialty: 'Ophthalmology',
       description: 'Advanced eye care services including surgeries and treatments for various eye conditions.',
@@ -83,7 +82,7 @@ const Dbooking = () => {
       id: 7,
       name: 'Dr. Mark Wilson',
       specialization: 'Dentist',
-      image: 'https://img.freepik.com/free-photo/happy-doctor_23-2148153982.jpg',
+      image: 'https://plus.unsplash.com/premium_photo-1658506671316-0b293df7c72b?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       alt: 'Dr. Mark Wilson',
       specialty: 'Dental Care',
       description: 'Full range of dental services including cleanings, fillings, and cosmetic procedures.',
@@ -93,7 +92,7 @@ const Dbooking = () => {
       id: 8,
       name: 'Dr. Linda Taylor',
       specialization: 'Gynecologist',
-      image: 'https://img.freepik.com/free-photo/portrait-smiling-female-doctor_23-2148849776.jpg',
+      image: 'https://images.unsplash.com/photo-1559839734-2b71ea197ec2?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       alt: 'Dr. Linda Taylor',
       specialty: 'Gynecology',
       description: 'Comprehensive women’s health services including prenatal care and gynecological surgeries.',
@@ -103,7 +102,7 @@ const Dbooking = () => {
       id: 9,
       name: 'Dr. Robert Brown',
       specialization: 'Neurologist',
-      image: 'https://img.freepik.com/free-photo/doctor-smiling-with-stethoscope_23-2148887824.jpg',
+      image: 'https://as1.ftcdn.net/v2/jpg/03/13/77/82/1000_F_313778250_Y0o5can6MA490Nt7G6p03Zfu5fKmWCIv.jpg',
       alt: 'Dr. Robert Brown',
       specialty: 'Neurology',
       description: 'Expert diagnosis and treatment of neurological disorders, including headaches and epilepsy.',
@@ -113,7 +112,7 @@ const Dbooking = () => {
       id: 10,
       name: 'Dr. Laura Green',
       specialization: 'Endocrinologist',
-      image: 'https://img.freepik.com/free-photo/portrait-smiling-female-doctor_23-2148849776.jpg',
+      image: 'https://plus.unsplash.com/premium_photo-1658506671316-0b293df7c72b?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       alt: 'Dr. Laura Green',
       specialty: 'Endocrinology',
       description: 'Specialized care for hormone-related conditions and disorders such as diabetes and thyroid issues.',
@@ -123,7 +122,7 @@ const Dbooking = () => {
       id: 11,
       name: 'Dr. Michael Adams',
       specialization: 'Hematologist',
-      image: 'https://img.freepik.com/free-photo/doctor-smiling-with-arms-crossed_23-2148170980.jpg',
+      image: 'https://images.unsplash.com/photo-1559839734-2b71ea197ec2?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       alt: 'Dr. Michael Adams',
       specialty: 'Hematology',
       description: 'Expert care for blood disorders, including anemia, leukemia, and clotting issues.',
@@ -133,7 +132,7 @@ const Dbooking = () => {
       id: 12,
       name: 'Dr. Alice Cooper',
       specialization: 'Rheumatologist',
-      image: 'https://img.freepik.com/free-photo/portrait-smiling-female-doctor_23-2148849776.jpg',
+      image: 'https://media.istockphoto.com/id/675005034/photo/young-smiling-confident-female-doctor-healthcare-professional-with-stethoscope-standing.jpg?s=612x612&w=0&k=20&c=9oou8xYRKw4TkiET_ztzQpZD-Io0ErIVsnjdddRNMOQ=',
       alt: 'Dr. Alice Cooper',
       specialty: 'Rheumatology',
       description: 'Diagnosis and treatment of autoimmune diseases and inflammatory conditions affecting the joints and muscles.',
@@ -143,7 +142,7 @@ const Dbooking = () => {
       id: 13,
       name: 'Dr. David Lee',
       specialization: 'Gastroenterologist',
-      image: 'https://img.freepik.com/free-photo/happy-doctor_23-2148153982.jpg',
+      image: 'https://images.unsplash.com/photo-1537368910025-700350fe46c7?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       alt: 'Dr. David Lee',
       specialty: 'Gastroenterology',
       description: 'Comprehensive care for digestive system disorders, including IBS, liver diseases, and more.',
@@ -153,7 +152,7 @@ const Dbooking = () => {
       id: 14,
       name: 'Dr. Nancy Wilson',
       specialization: 'Infectious Disease Specialist',
-      image: 'https://img.freepik.com/free-photo/portrait-smiling-female-doctor_23-2148849776.jpg',
+      image: 'https://as1.ftcdn.net/v2/jpg/03/13/77/82/1000_F_313778250_Y0o5can6MA490Nt7G6p03Zfu5fKmWCIv.jpg',
       alt: 'Dr. Nancy Wilson',
       specialty: 'Infectious Diseases',
       description: 'Specialized treatment for infections caused by bacteria, viruses, fungi, and parasites.',
@@ -163,7 +162,7 @@ const Dbooking = () => {
       id: 15,
       name: 'Dr. Charles King',
       specialization: 'Pulmonologist',
-      image: 'https://img.freepik.com/free-photo/doctor-smiling-with-arms-crossed_23-2148170980.jpg',
+      image: 'https://images.unsplash.com/photo-1585842378054-ee2e52f94ba2?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       alt: 'Dr. Charles King',
       specialty: 'Pulmonology',
       description: 'Care for respiratory conditions, including asthma, COPD, and lung infections.',
@@ -173,7 +172,7 @@ const Dbooking = () => {
       id: 16,
       name: 'Dr. Olivia Martinez',
       specialization: 'Urologist',
-      image: 'https://img.freepik.com/free-photo/portrait-smiling-female-doctor_23-2148849776.jpg',
+      image: 'https://media.istockphoto.com/id/675005034/photo/young-smiling-confident-female-doctor-healthcare-professional-with-stethoscope-standing.jpg?s=612x612&w=0&k=20&c=9oou8xYRKw4TkiET_ztzQpZD-Io0ErIVsnjdddRNMOQ=',
       alt: 'Dr. Olivia Martinez',
       specialty: 'Urology',
       description: 'Expert treatment for urinary tract and male reproductive system disorders.',
@@ -183,7 +182,7 @@ const Dbooking = () => {
       id: 17,
       name: 'Dr. James Wilson',
       specialization: 'Plastic Surgeon',
-      image: 'https://img.freepik.com/free-photo/doctor-smiling-with-arms-crossed_23-2148170980.jpg',
+      image: 'https://media.istockphoto.com/id/1494324933/photo/portrait-of-asian-chinese-mixed-race-senior-mature-man-doctor-in-collared-business-shirt.webp?s=2048x2048&w=is&k=20&c=kipGtD1Tfd4y49mdCoCoE6Hpo3FSlP9anUIjkqvp_Ys=',
       alt: 'Dr. James Wilson',
       specialty: 'Plastic Surgery',
       description: 'Cosmetic and reconstructive surgical procedures to enhance appearance and restore function.',
@@ -193,32 +192,13 @@ const Dbooking = () => {
       id: 18,
       name: 'Dr. Sophia Lee',
       specialization: 'Oncologist',
-      image: 'https://img.freepik.com/free-photo/portrait-smiling-female-doctor_23-2148849776.jpg',
+      image: 'https://images.unsplash.com/photo-1559839734-2b71ea197ec2?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       alt: 'Dr. Sophia Lee',
       specialty: 'Oncology',
       description: 'Comprehensive care for cancer patients, including diagnosis, treatment, and follow-up care.',
       bookingLink: './appointment?doctor=SophiaLee'
     },
-    'WilliamScott': {
-      id: 19,
-      name: 'Dr. William Scott',
-      specialization: 'Nephrologist',
-      image: 'https://img.freepik.com/free-photo/doctor-smiling-with-stethoscope_23-2148887824.jpg',
-      alt: 'Dr. William Scott',
-      specialty: 'Nephrology',
-      description: 'Specialized care for kidney diseases, including dialysis and transplant management.',
-      bookingLink: './appointment?doctor=WilliamScott'
-    },
-    'IsabellaGarcia': {
-      id: 20,
-      name: 'Dr. Isabella Garcia',
-      specialization: 'Psychiatrist',
-      image: 'https://img.freepik.com/free-photo/portrait-smiling-female-doctor_23-2148849776.jpg',
-      alt: 'Dr. Isabella Garcia',
-      specialty: 'Psychiatry',
-      description: 'Comprehensive mental health care, including therapy, medication management, and counseling.',
-      bookingLink: './appointment?doctor=IsabellaGarcia'
-    }
+    
   };
   
 
@@ -303,7 +283,6 @@ const Dbooking = () => {
     return `${day} ${monthName} ${year}`;
   }
 
-
   return (
     <div>
       <div className='appointment'>
@@ -370,3 +349,4 @@ const Dbooking = () => {
 };
 
 export default Dbooking;
+
