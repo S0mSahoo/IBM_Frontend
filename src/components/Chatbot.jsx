c6e14383ced589b04e1a84a3d6b9fb9d1c2f7904
Satyajeet-Jena <satyajeetjena2111100652@gmail.com>
2024-08-02 18:27:59 +0530

    Added preliminary styling for chatbox box
    - cross button for back
    - randomise questions

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -10,6 +10,18 @@ const Chatbot = () => {
 	const b = document.createElement("div");
 	b.className = "chat-exit-button";
 
+	const c  = document.createElement("div");
+	c.className = "chat-exit-button-filler1";
+
+	let d = document.createElement("div");
+	d.className = "chat-exit-button-filler2";
+	d.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="royalblue" class="bi bi-x-square" viewBox="0 0 16 16">
+  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+</svg>`
+	b.appendChild(c);
+	b.appendChild(d);
+
 	b.addEventListener("click", (event) => {
 	
 	  let chat_exp = document.getElementsByClassName("expanded")[0];
