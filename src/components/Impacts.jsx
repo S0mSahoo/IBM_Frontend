ad4a955f2a512036e9a48d3ca1e2cf6261a187b7
S0mSahoo <sompriyansu308@gmail.com>
2024-08-09 13:40:25 +0530

    Update Impacts.jsx

--- src/components/Impacts.jsx
+++ src/components/Impacts.jsx
@@ -12,7 +12,7 @@ const Impacts = () => {
     window.scrollTo(0, 0)
   }, [])
   return( 
-  <div className="bg-img flex flex-col w-[100vw] min-h-[100vh] justify-center items-center bg-gray-200 overflow-x-hidden overflow-y-scroll pt-[100px] pb-4" style={scrollBar}  data-aos='fade-up'>
+  <div className="bg-img flex flex-col w-[100vw] min-h-[100vh] justify-center items-center bg-gray-200 overflow-x-hidden overflow-y-scroll pt-[100px] pb-10" style={scrollBar}  data-aos='fade-up'>
     <div className="text-center">
       <h1 className="text-4xl font-bold"><span className="text-royalblue">Impact</span> Stories</h1>
       <div className="bg-theme h-[4px] w-1/5 mt-1 mx-auto"></div>
